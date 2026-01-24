28157a0bc4a647d7626dad31dde32dd2e35624c2
parwatsinghpanwar <parwatpanwar8316@gmail.com>
2026-01-25 02:52:56 +0530

    Refactor Resumebuilder component to include state management and resume loading functionality

--- client/src/pages/Resumebuilder.jsx
+++ client/src/pages/Resumebuilder.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { dummyResumeData } from '../assets/assets';
+import { Link, useParams } from 'react-router-dom';
+import { ArrowLeft, ArrowLeftIcon } from 'lucide-react';
 
 const Resumebuilder = () => {
-  return (
-    <div>
-        <h1>Resumebuilder</h1>
+    const { ResumeId } = useParams();
+    const [resumeData, setResumeData] = useState({
+        id: '',
+        tile: '',
+        personal_info: {},
+        perofessional_summary: '',
+        experience: [],
+        education: [],
+        project: [],
+        skills: [],
+        template: "classic",
+        accent_color: '#3B82F6',
+        public: false,
+    });
+    const looadExistingResume = async () => {
+        const resume = dummyResumeData.find(resume => resume.id === ResumeId);
+        if (resume) {
+            setResumeData(resume);
+            document.title = resume.title;
+        }
+        useEffect(() => {
+            looadExistingResume();
+        }, []);
+    }
+    return (
+        <div>
+       <div className='max-w-7xl max-auto px-4 py-6  '>
+        <Link to={'/App'} className='inline-flex gap-2 items-center text-slate-500  hover:text-slate-700 transition-all'> 
+        <ArrowLeftIcon className='size-4' />Back to Dashboard
+        </Link>
+       </div>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Resumebuilder
